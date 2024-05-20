8ee3ff67427778d5c3f040aa211eda7c514a56fd
Darpan <darpandeka6000@gmail.com>
2024-05-20 14:27:02 +0530

    new solution added

---
+++ LC1863.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution {
+public:
+#define ll long long
+#define nn "\n" 
+#define fr(x,y) for(ll x=0; x<y; x++)
+#define fr1(x,t,y) for(ll x=t; x<y; x++)
+#define fr3(x,t,y) for(ll x=t; x>y; x--)
+#define ga(a,n) ll a[n]; fr(i,n) cin>>a[i];
+#define gv(v,n) vll v(n); fr(i,n) cin>>v[i];
+#define vll vector <ll>
+#define vpair vector<pair<ll,ll>>
+#define pb push_back
+#define pp pop_back
+#define ff first
+#define ss second
+#define yes cout<<"YES"<<nn
+#define no cout<<"NO"<<nn
+#define all(vector) vector.begin(), vector.end()
+#define print(v) fr(i,v.size()) cout<<v[i]<<" "; cout<<nn;
+const ll mod = 1e9+7;
+    ll n;
+    vector<int> a;
+    ll rec(ll i, ll x){
+        if(i==n) return x;
+        ll ans = rec(i+1,x);
+        ans += rec(i+1,a[i]^x);
+        return ans;
+    }
+    int subsetXORSum(vector<int>& nums) {
+        n=nums.size();
+        a=nums;
+        return rec(0,0);
+    }
+};
