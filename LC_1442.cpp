dd6229691abf25183b260bdc74447baa0d3fcfd5
Darpan <darpandeka6000@gmail.com>
2024-05-31 00:32:16 +0530

    LC 1442

---
+++ LC_1442.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution {
+public:
+#define ll long long
+#define nn "\n" 
+#define fr(x,y) for(ll x=0; x<y; x++)
+#define fr1(x,t,y) for(ll x=t; x<y; x++)
+#define fr3(x,t,y) for(ll x=t; x>y; x--)
+#define ga(a,n) ll a[n]; fr(i,n) cin>>a[i];
+#define gv(v,n) vll v(n); fr(i,n) cin>>v[i];
+#define vll vector <ll>
+#define vpair vector<pair<ll,ll>>
+#define pb push_back
+#define pp pop_back
+#define ff first
+#define ss second
+#define yes cout<<"YES"<<nn
+#define no cout<<"NO"<<nn
+#define all(vector) vector.begin(), vector.end()
+#define print(v) fr(i,v.size()) cout<<v[i]<<" "; cout<<nn;
+const ll mod = 1e9+7;
+    ll n;
+    int countTriplets(vector<int>& a) {
+        n = a.size();
+        vll px(n);
+        fr(i,n){
+            px[i] = a[i];
+            if(i) px[i] ^= px[i-1];
+        }
+        print(px);
+        ll ans=0;
+        fr(i,n){
+            for(ll j=i+1; j<n; j++){
+                if((i==0 && px[j]==0)){
+                    ans += j-i;
+                }
+                else if(i && px[i-1] == px[j]){
+                    ans += j-i;
+                }
+            }
+        }
+        return ans;
+    }
+};
