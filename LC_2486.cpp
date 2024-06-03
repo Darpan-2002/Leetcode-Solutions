3a8e58ad414a287e288f5df31c6e2a373f7878e8
Darpan <darpandeka6000@gmail.com>
2024-06-03 11:20:41 +0530

    LC 2486

---
+++ LC_2486.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nn "\n" 
+#define fr(x,y) for(ll x=0; x<y; x++)
+#define fr1(x,t,y) for(ll x=t; x<y; x++)
+#define fr3(x,t,y) for(ll x=t; x>y; x--)
+#define ga(a,n) ll a[n]; fr(i,n) cin>>a[i];
+#define gv(v,n) vll v(n); fr(i,n) cin>>v[i];
+#define vll vector <ll>
+#define vpair vector<pair<ll,ll>>
+#define pb push_back
+#define pp pop_back
+#define ff first
+#define ss second
+#define yes cout<<"YES"<<nn
+#define no cout<<"NO"<<nn
+#define all(vector) vector.begin(), vector.end()
+#define print(v) fr(i,v.size()) cout<<v[i]<<" "; cout<<nn;
+const ll mod = 1e9+7;
+
+
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        ll n=s.size(), m=t.size();
+        ll j=0;
+        fr(i,n){
+            if(s[i] == t[j]) j++;
+            if(j==m) break;
+        }
+        return m-j;
+    }
+};
