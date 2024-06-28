a0242dd1315d5d937bb85d14b381c9cd738c9668
Darpan <darpandeka6000@gmail.com>
2024-06-28 11:54:56 +0530

    LC 2285

---
+++ LC_2285.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nn "\n" 
+#define fr(x,y) for(ll x=0; x<y; x++)
+#define fr1(x,t,y) for(ll x=t; x<y; x++)
+#define fr3(x,t,y) for(ll x=t; x>y; x--)
+#define ga(a,n) ll a[n]; fr(i,n) cin>>a[i];
+#define gv(v,n) vll v(n); fr(i,n) cin>>v[i];
+#define vll vector <ll>
+#define vpair vector<pair<ll,ll>>
+#define pb push_back
+#define pp pop_back
+#define ff first
+#define ss second
+#define yes cout<<"YES"<<nn
+#define no cout<<"NO"<<nn
+#define all(vector) vector.begin(), vector.end()
+#define print(v) fr(i,v.size()) cout<<v[i]<<" "; cout<<nn;
+const ll mod = 1e9+7;
+
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        ll si = roads.size();
+        vll deg(n,0);
+        vector<vll> adj(n);
+        for(auto it:roads){
+            adj[it[0]].pb(it[1]);
+            adj[it[1]].pb(it[0]);
+            deg[it[0]]++;
+            deg[it[1]]++;
+        }
+        sort(all(deg));
+        ll ans=0, j=1;
+        fr(i,n){
+            ans += deg[i]*(j++);
+        }
+        return ans;
+    }
+};
