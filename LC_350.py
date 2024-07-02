920fea0bf7ac600580d28bad8f7042fc65b033ef
Darpan <darpandeka6000@gmail.com>
2024-07-03 01:56:52 +0530

    LC 350

---
+++ LC_350.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1.sort()
+        nums2.sort()
+        n = len(nums1); m = len(nums2)
+        i = 0; j = 0
+        ans = []
+        while i<n and j<m:
+            if nums1[i]==nums2[j]:
+                ans.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                i += 1
+        return ans
